4b387a72b07d39f35f55d3169fbf057f16ab2563
ahmadkmal <eng.ahmadkmal@gmail.com>
2020-09-19 01:50:10 +0300

    add style

--- src/components/body/index.js
+++ src/components/body/index.js
@@ -2,31 +2,30 @@ import React, { useEffect, useState } from 'react';
 import Card from 'react-bootstrap/Card';
 import { useHistory } from "react-router-dom";
 import Auth from '../auth';
+import './body.scss';
 const Body = (props) => {
     const history = useHistory();
     return (
-        <div>
-            <div>
-                <img src={props.data.userImage || 'holder'} />
+        <div className="mainodydiv">
+            <div className="firstdiv">
+                <img className="postimg" src={props.data.userImage || 'holder'} alt="" />
                 <p>{props.data.username}</p>
                 <p>{props.data.date}</p>
             </div>
-
-            <div>
+            <div className="seconddiv">
                 <p>
                     {props.data.body}
                 </p>
             </div>
             <Auth capability={'admin'} owner={props.data.username}>
                 <img 
+                className="adding"
                 src='https://upload.wikimedia.org/wikipedia/commons/thumb/6/64/Edit_icon_%28the_Noun_Project_30184%29.svg/1200px-Edit_icon_%28the_Noun_Project_30184%29.svg.png' 
                 alt='edit' 
                 onClick={()=>{history.push(`/body/${props.data._id}`);}}
                 />
             </Auth>
-
         </div>
     )
-
 }
 export default Body;
--- src/components/editBody/index.js
+++ src/components/editBody/index.js
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from 'react';
 import { useHistory } from "react-router-dom";
 import { useParams } from 'react-router-dom';
 import superagent from 'superagent';
+import axios from 'axios';
 import Auth from '../auth';
 import Form from 'react-bootstrap/Form';
 import { connect } from 'react-redux';
@@ -9,48 +10,66 @@ import { connect } from 'react-redux';
 const API = process.env.API_URL || 'https://blog-pwc.herokuapp.com';
 const EditBody = (props) => {
     const history = useHistory();
+    const [body, setBody] = useState('');
     const [data, setData] = useState({});
-    const [loader, setLoader] = useState(false);
     let { id } = useParams();
     useEffect(() => {
-        setLoader(true);
         console.log("post data--->");
         (async () => {
             await superagent.get(`${API}/body/${id}`)
                 .set('Content-Type', 'application/json')
-                .set('Authorization',`Bearer ${props.token}`)
+                .set('Authorization', `Bearer ${props.token}`)
                 .then(data => {
                     setData(data.body);
-                    setLoader(false);
+                    setBody(data.body.body);
                     console.log("post data--->", data.body)
                 }).catch(err => console.log(err));
         })();
 
 
     }, []);
+    const handleSubmit = async e => {
+        e.preventDefault();
+        let api = `https://blog-pwc.herokuapp.com/body/${id}`;
+        const options = {
+          mode: 'cors',
+          headers: { 'Content-Type': 'application/json' ,'Authorization': `Bearer ${props.token}`},
+          cache: 'no-cache',
+        };
+        console.log('axios--->',{body})
+        axios.put(api,{body},options)
+          .then(data => {
+              console.log('add post --->',data);
+              history.goBack();
+            }
+          ).catch(err=>console.log(err));
+        
+      };
     return (
         <div>
-            <Auth capability={'admin'} owner={data.username||''}>
-            <div>
-                <img src={data.userImage || 'holder'} />
-                <p>{data.username}</p>
-                <p>{data.date}</p>
-            </div>
+            <Auth capability={'admin'} owner={data.username || ''}>
+                <div>
+                    <img src={data.userImage || 'holder'} />
+                    <p>{data.username}</p>
+                    <p>{data.date}</p>
+                </div>
 
-            <div>
-                <p>
-                    {data.body}
-                </p>
-                <Form>
-            <Form.Group controlId="exampleForm.ControlTextarea1">
-                <Form.Label>edit this body</Form.Label>
-                <Form.Control as="textarea" rows="3" value={data.body}/>
-            </Form.Group>
-            <button type="submit" className="btn btn-primary btn-block">Add</button>
-        </Form>
-            </div>
-            
-               
+                <div>
+                    <p>
+                        {data.body}
+                    </p>
+                    <Form onSubmit={handleSubmit}>
+                        <Form.Group controlId="exampleForm.ControlTextarea1">
+                            <Form.Label>edit this body</Form.Label>
+                            <Form.Control as="textarea" rows="3"
+                                value={body}
+                                onChange={e => setBody(e.target.value)} />
+                        </Form.Group>
+                        <button type="submit" className="btn btn-primary btn-block">ُEdit</button>
+                    </Form>
+                </div>
+
+
             </Auth>
 
         </div>
@@ -61,7 +80,6 @@ const mapStateToProps = (state) => {
     return {
         token: state.auth.token,
     };
-  };
-  
-  export default connect(mapStateToProps)(EditBody);
-  
+};
+
+export default connect(mapStateToProps)(EditBody);
